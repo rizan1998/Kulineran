386b2457f052ba173adcaa7aff299ba1f8d8ab29
rizan1998 <rizanalfalah@gmail.com>
2021-01-26 16:20:58 +0700

    berhasil mengambil data menggunakan axios

---
+++ src/components/UserComponent/CardProduct.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cardproduct">
+    <div class="card" style="width: 18rem">
+      <img
+        class="card-img-top"
+        :src="'assets/images/' + product.gambar"
+        alt="Card image cap"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{ product.nama }}</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardProduct",
+  props: ["product"],
+};
+</script>
--- src/views/User/Home.vue
+++ src/views/User/Home.vue
@@ -3,6 +3,25 @@
     <NavbarComponent />
     <div class="container">
       <Hero />
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong>Foods</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye class="mr-1"></b-icon-eye>Lihat semua
+          </router-link>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <div
+          class="col-md-4 mt-4"
+          v-for="product of products"
+          :key="product.id"
+        >
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
     <FooterComponent />
   </div>
@@ -12,6 +31,9 @@
 import NavbarComponent from "../../components/UserComponent/Navbar";
 import Hero from "../../components/UserComponent/Hero";
 import FooterComponent from "../../components/UserComponent/Footer";
+import CardProduct from "../../components/UserComponent/CardProduct";
+
+import axios from "axios";
 
 export default {
   name: "Home",
@@ -19,6 +41,33 @@ export default {
     NavbarComponent,
     Hero,
     FooterComponent,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    //ketika halaman dipasang makan kodingannya berjalan
+
+    // Make a request for a user with a given ID
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => {
+        // handle success
+        console.log("berhasil:", response);
+        this.setProduct(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   },
 };
 </script>
