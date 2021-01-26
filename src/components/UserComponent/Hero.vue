b7563e6a735aa726c3f0a4cf415873584bd65c47
rizan1998 <rizanalfalah@gmail.com>
2021-01-26 15:43:43 +0700

    mengubah hero pada mobile version

--- src/components/UserComponent/Hero.vue
+++ src/components/UserComponent/Hero.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Delicious Food Menu</strong>
-            <p>In Your Gedget</p>
-          </h2>
-          <p>Ayo Segera Pilih dan pesan makanan favorit anda</p>
-          <button to="/Pesan" class="btn btn-lg btn-success">
-            <b-icon-arrow-right></b-icon-arrow-right> Pesan
-          </button>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-md-block d-none">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu</strong>
+                <p>In Your Gedget</p>
+              </h2>
+              <p>Ayo Segera Pilih dan pesan makanan favorit anda</p>
+              <button to="/Pesan" class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../../assets/images/hero.png" width="100%" alt="" />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../../assets/images/hero.png" width="100%" alt="" />
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <img src="../../assets/images/hero.png" width="100%" alt="" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu</strong>
+                <p>In Your Gedget</p>
+              </h2>
+              <p>Ayo Segera Pilih dan pesan makanan favorit anda</p>
+              <button to="/Pesan" class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
