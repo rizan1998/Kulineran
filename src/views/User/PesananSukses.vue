1f6785e7fb3dafa934a743ad78978683ab007d26
rizan1998 <rizanalfalah@gmail.com>
2021-01-27 16:47:38 +0700

    menambah json database dan memperbaiki halaman pemesanan sukses

--- src/views/User/PesananSukses.vue
+++ src/views/User/PesananSukses.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row justify-content-center mt-5">
         <div class="col text-center">
-          <img src="../assets/images/menunggu.png" width="300" />
+          <img src="../../assets/images/menunggu.png" width="300" />
           <h2><strong>Sukses!!</strong></h2>
           <h4>
             Pesanan Anda segera diproses <br />
