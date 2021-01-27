f35b2b0b324d0b4f8689c340bc006dcbef96f5c1
rizan1998 <rizanalfalah@gmail.com>
2021-01-27 12:49:36 +0700

    berhasil menambahkan data ke keranjang dengan menggunakan axios, dan berhasil memakai plugins vue toast untuk nontification data bertambah

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
+Vue.use(VueToast);
+
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
--- src/views/User/FoodDetail.vue
+++ src/views/User/FoodDetail.vue
@@ -2,21 +2,62 @@
   <div class="FoodDetail">
     <NavbarComponent />
     <div class="container">
-      <div class="row">
+      <div class="row mt-4">
         <div class="col">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item">
-                <router-link to="/">Home</router-link>
+                <router-link class="text-dark" to="/">Home</router-link>
               </li>
               <li class="breadcrumb-item">
-                <router-link to="/Foods">Foods</router-link>
+                <router-link to="/Foods" class="text-dark">Foods</router-link>
               </li>
               <li class="breadcrumb-item active" aria-current="page">
                 Food Order
               </li>
             </ol>
           </nav>
+        </div>
+      </div>
+
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <img
+            :src="'../assets/images/' + product.gambar"
+            class="img-fluid shadow"
+          />
+        </div>
+        <div class="col-md-6">
+          <h2>
+            <strong>{{ product.nama }}</strong>
+          </h2>
+          <hr />
+          <h4>
+            Harga :
+            <strong>Rp. {{ product.harga }}</strong>
+          </h4>
+          <form class="mt-4" v-on:submit.prevent>
+            <div class="form-group">
+              <label for="jumlah_pemesanan">Jumlah Pesan</label>
+              <input
+                type="number"
+                class="form-control"
+                v-model="pesan.jumlah_pemesanan"
+              />
+            </div>
+            <div class="form-group">
+              <label for="keterangan">Keterangan</label>
+              <textarea
+                v-model="pesan.keterangan"
+                class="form-control"
+                placeholder="Keterangan spt : Pedes, Nasi Setengah .."
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-success" @click="pemesanan">
+              <b-icon-cart></b-icon-cart>Pesan
+            </button>
+          </form>
         </div>
       </div>
     </div>
@@ -33,33 +74,40 @@ export default {
     NavbarComponent,
     FooterComponent,
   },
-  props: ["id"],
   data() {
     return {
-      products: [],
-      search: "",
+      product: {},
+      pesan: {},
     };
   },
   methods: {
     setProduct(data) {
       // console.log(data);
-      this.products = data;
+      this.product = data;
     },
-    searchFoods() {
-      axios
-        .get("http://localhost:3000/products?q=" + this.search)
-        .then((response) => {
-          // handle success
-          // console.log("berhasil:", response);
-          if (response.data.length == 0) {
-            this.setProduct(null);
-          }
-          this.setProduct(response.data);
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
+    pemesanan() {
+      if (this.pesan.jumlah_pemesanan) {
+        this.pesan.products = this.product;
+        axios
+          .post("http://localhost:3000/keranjangs", this.pesan)
+          .then(() => {
+            // this.$router.push({ path: "/keranjang" });
+            this.$toast.success("Sukses Masuk Keranjang", {
+              type: "success",
+              position: "top-right",
+              duration: 3000,
+              dismissible: true,
+            });
+          })
+          .catch((err) => console.log(err));
+      } else {
+        this.$toast.error("Jumlah Pesanan Harus diisi", {
+          type: "error",
+          position: "top-right",
+          duration: 3000,
+          dismissible: true,
         });
+      }
     },
   },
   mounted() {
@@ -67,7 +115,7 @@ export default {
 
     // Make a request for a user with a given ID
     axios
-      .get("http://localhost:3000/products")
+      .get("http://localhost:3000/products/" + this.$route.params.id)
       .then((response) => {
         // handle success
         console.log("berhasil:", response);
